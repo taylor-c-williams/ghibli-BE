af1315895221b38682ac347f02c8f030e0a4ba96
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-09-29 13:56:25 -0700

    initalized

---
+++ data/create-tables.js
@@ -0,0 +1,48 @@
+const client = require('../lib/client');
+const { getEmoji } = require('../lib/emoji.js');
+
+// async/await needs to run in a function
+run();
+
+async function run() {
+
+  try {
+    // initiate connecting to db
+    await client.connect();
+
+    // run a query to create tables
+    await client.query(`
+                CREATE TABLE users (
+                    id SERIAL PRIMARY KEY,
+                    email VARCHAR(256) NOT NULL,
+                    hash VARCHAR(512) NOT NULL
+                );           
+                CREATE TABLE films (
+                    id SERIAL PRIMARY KEY NOT NULL,
+                    title VARCHAR(512) NOT NULL,
+                    original_title VARCHAR(512) NOT NULL,
+                    description VARCHAR(512) NOT NULL,
+                    director VARCHAR(512) NOT NULL,
+                    producer VARCHAR(512) NOT NULL,
+                    release_date INTEGER NOT NULL,
+                    running_time INTEGER NOT NULL,
+                    rt_score INTEGER NOT NULL,
+                    img VARCHAR(512) NOT NULL,
+                    miyazaki VARCHAR(512) NOT NULL,
+                    category VARCHAR(512) NOT NULL,
+                    owner_id INTEGER NOT NULL REFERENCES users(id)
+            );
+        `);
+
+    console.log('create tables complete', getEmoji(), getEmoji(), getEmoji());
+  }
+  catch(err) {
+    // problem? let's see the error...
+    console.log(err);
+  }
+  finally {
+    // success or failure, need to close the db connection
+    client.end();
+  }
+
+}
---
+++ data/load-seed-data.js
@@ -0,0 +1,47 @@
+const client = require('../lib/client');
+// import our seed data:
+const films = require('./films.js');
+const usersData = require('./users.js');
+const { getEmoji } = require('../lib/emoji.js');
+
+run();
+
+async function run() {
+
+  try {
+    await client.connect();
+
+    const users = await Promise.all(
+      usersData.map(user => {
+        return client.query(`
+                      INSERT INTO users (email, hash)
+                      VALUES ($1, $2)
+                      RETURNING *;
+                  `,
+        [user.email, user.hash]);
+      })
+    );
+      
+    const user = users[0].rows[0];
+
+    await Promise.all(
+      films.map(film => {
+        return client.query(`
+                    INSERT INTO films (title, original_title, description, director, producer, release_date, running_time, rt_score, img, miyazaki, category)
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);
+                `,
+        [film.name, film.cool_factor, user.id]);
+      })
+    );
+    
+
+    console.log('seed data load complete', getEmoji(), getEmoji(), getEmoji());
+  }
+  catch(err) {
+    console.log(err);
+  }
+  finally {
+    client.end();
+  }
+    
+}
